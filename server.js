5ce3b7641cb3d94e5a2f976a4d22098619c954d0
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-10-04 18:40:33 +0200

    Fix routes import

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors')
 const path = require('path')
+const testimonialsRoutes = require('./routes/testimonials.routes');
+const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -10,20 +14,18 @@ app.use(cors({
 }));
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client/build')));
-// import routes
-const testimonialsRoutes = require('./routes/testimonials.routes');
+// use routes
 app.use('/api', testimonialsRoutes);
-const concertsRoutes = require('./routes/concerts.routes');
 app.use('/api', concertsRoutes);
-const seatsRoutes = require('./routes/seats.routes');
 app.use('/api', seatsRoutes);
+// Serve main react app
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
+// Handle bad requests
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
 })
-
 // Start server
 app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
